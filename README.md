a182384ee04a84251c511fde811e4d7675fd6bda
Ahmed Fasih <wuzzyview@gmail.com>
2021-12-20 10:27:53 -0800

    fix NYC, DC, and San Joaquin

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # Covid County
-## setup
+
+## Setup
 ```console
 conda create  -n covid-county
 conda activate covid-county
 conda install pandas ipython
 ```
+
 ## 2020 US presidential election
-https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/VOQCHQ
+Download `countypres_2000-2020.csv` from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/VOQCHQ
+
+2020 data is missing counts for District of Columbia (FIPS 11001)? Party split taken from 2016 election.
+
 ## Covid
-git clone --depth 10 https://github.com/nytimes/covid-19-data.git
+Install Git and run this in this directory: `git clone --depth 1 https://github.com/nytimes/covid-19-data.git` (it might take a while)
+
+Note five boroughs of NYC are combined into a single "county". This is taken into account by merging the 2020 Presidential votes from all five boroughs into a single county (since we can't split the Covid deaths into individual boroughs, this is the best we can do).
+
+## Run
+```
+python main.py
+```
+
+## Results
+| party bin | total Covid-19 deaths |
+|---|---|
+| Dem <-20% |  38284| 
+| Dem <-40% | 211416| 
+| Dem <-50% | 123587| 
+| Dem <+60% | 196084| 
+| Dem <+80% | 210070| 
+| Dem >+80% |  18331| 
+| unknown   |   5243| 
+
+- Dem: 424485
+- Rep: 373287
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import pandas as pd  # type: ignore
+import numpy as np
 
 county_to_party = dict()
 
@@ -10,13 +11,48 @@ for fips, bycounty in latest.groupby(by='county_fips'):
   dem = byparty.DEMOCRAT / total
   county_to_party[fips] = dem
 
+# To handle DC data missing for 2020, get party votes from 2016
+DISTRICT_OF_COLUMBIA_FIPS = 11001
+if DISTRICT_OF_COLUMBIA_FIPS not in county_to_party:
+  dc = pres[np.logical_and(pres.year == 2016, pres.county_fips == 11001)]
+  dem = dc.loc[dc.party == 'DEMOCRAT']
+  county_to_party[DISTRICT_OF_COLUMBIA_FIPS] = float(dem.candidatevotes / dem.totalvotes)
+# Also SAN JOAQUIN, CA (fips 06077) is missing total
+SAN_JOAQUIN_CA_FIPS = 6077
+if pd.isna(county_to_party[SAN_JOAQUIN_CA_FIPS]):
+  g = latest[latest.county_fips == SAN_JOAQUIN_CA_FIPS]
+  total = g.candidatevotes.sum()
+  county_to_party[SAN_JOAQUIN_CA_FIPS] = float(g.loc[g.party == 'DEMOCRAT'].candidatevotes / total)
+# All five counties in NYC are grouped together
+NYT_FAKE_NYC_FIPS = 36998.0
+if NYT_FAKE_NYC_FIPS not in county_to_party:
+  dem = 0
+  total = 0
+  NYC_FIPS = [36005, 36047, 36061, 36081, 36085]
+  for fips in NYC_FIPS:
+    row = latest[np.logical_and(latest.county_fips == fips, latest.party == 'DEMOCRAT')]
+    dem += float(row.candidatevotes.sum())
+    total += float(row.totalvotes)
+  county_to_party[NYT_FAKE_NYC_FIPS] = dem / total
+
+## covid
 covid = pd.concat([
     pd.read_csv('covid-19-data/rolling-averages/us-counties-2020.csv'),
     pd.read_csv('covid-19-data/rolling-averages/us-counties-2021.csv')
 ])
 
-geoid_to_deaths = covid.groupby(['geoid'])['deaths'].sum()
+geoid_to_deaths = covid.groupby(['geoid'])['deaths'].sum().to_frame()
+geoid_to_deaths['county'] = covid.groupby(['geoid'])['county'].max()
+geoid_to_deaths['state'] = covid.groupby(['geoid'])['state'].max()
 geoid_to_deaths.index = [float(x.split('-')[1]) for x in geoid_to_deaths.index]
+geoid_to_deaths['dem'] = [
+    county_to_party[fips] if fips in county_to_party else pd.NA for fips in geoid_to_deaths.index
+]
+
+MISSING_VOTE_DATA = geoid_to_deaths[np.logical_and(
+    pd.isna(geoid_to_deaths.dem), geoid_to_deaths.deaths > 0)].sort_values('deaths')
+
+print(MISSING_VOTE_DATA)
 
 
 def bin_names(edges=(0.2, 0.4, 0.5, 0.6, 0.8)) -> list[str]:
@@ -29,67 +65,18 @@ def pct_to_bin(pct: float, edges=(0.2, 0.4, 0.5, 0.6, 0.8)):
 
 
 bins = bin_names()
-partydf = pd.DataFrame.from_dict(county_to_party, orient='index', columns=['dem'])
-partydf['bin'] = [bins[pct_to_bin(x)] for x in partydf.dem]
-partydf['deaths'] = [
-    geoid_to_deaths.loc[fips] if fips in geoid_to_deaths else -1 for fips in county_to_party
+geoid_to_deaths['bin'] = [
+    bins[pct_to_bin(x)] if not pd.isna(x) else 'unknown' for x in geoid_to_deaths.dem
 ]
 
-print('area under curve\n', partydf.groupby('bin')['deaths'].sum())
-"""
-Dem <-20%     40141
-Dem <-40%    211405
-Dem <-50%    123572
-Dem <+60%    194219
-Dem <+80%    174997
-Dem >+80%     17121
-"""
+geoid_to_deaths.sort_values('deaths')
 
-MISSING_FIPS = list(filter(lambda fips: fips not in geoid_to_deaths, county_to_party))
-print('MISSING', MISSING_FIPS)
-"""
-[2001.0,
- 2002.0,
- 2003.0,
- 2004.0,
- 2005.0,
- 2006.0,
- 2007.0,
- 2008.0,
- 2009.0,
- 2010.0,
- 2011.0,
- 2012.0,
- 2014.0,
- 2015.0,
- 2017.0,
- 2018.0,
- 2019.0,
- 2021.0,
- 2022.0,
- 2023.0,
- 2024.0,
- 2025.0,
- 2026.0,
- 2027.0,
- 2028.0,
- 2029.0,
- 2030.0,
- 2031.0,
- 2032.0,
- 2033.0,
- 2034.0,
- 2035.0,
- 2036.0,
- 2037.0,
- 2038.0,
- 2039.0,
- 2040.0,
- 2099.0,
- 36000.0,
- 36005.0,
- 36047.0,
- 36061.0,
- 36081.0,
- 36085.0,
- 46113.0]"""
+by_bin = geoid_to_deaths.groupby('bin')['deaths'].sum()
+print(by_bin)
+
+print('Dem', sum([by_bin.loc[x] if '+' in x else 0 for x in by_bin.index]), 'Rep',
+      sum([by_bin.loc[x] if '-' in x else 0 for x in by_bin.index]))
+
+by_party = geoid_to_deaths.dropna().groupby(
+    lambda x: geoid_to_deaths.loc[x].dem > 0.5)['deaths'].sum()
+print(by_party)
